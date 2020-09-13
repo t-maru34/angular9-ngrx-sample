111215d1c6bceeefa40ab92413fe7f666e52ea33
t-maru34 <49508287+t-maru34@users.noreply.github.com>
2020-09-13 21:08:38 +0900

    Bump up package versions (#13)
    * Update packages
    
    * Update ngrx to v9
    
    * Fix test

--- src/app/effects/core.effects.spec.ts
+++ src/app/effects/core.effects.spec.ts
@@ -3,17 +3,32 @@ import { provideMockActions } from '@ngrx/effects/testing';
 import { Observable } from 'rxjs';
 
 import { CoreEffects } from './core.effects';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
 
 describe('CoreEffects', () => {
   let actions$: Observable<any>;
   let effects: CoreEffects;
 
+  let snackBarSpy: jasmine.SpyObj<MatSnackBar>;
+  let dialogSpy: jasmine.SpyObj<MatDialog>;
+
   beforeEach(() => {
+    const snackbarSpyDef = jasmine.createSpyObj('snackBar', ['open']);
+    const dialogSpyDef = jasmine.createSpyObj('dialog', ['open']);
+
     TestBed.configureTestingModule({
-      providers: [CoreEffects, provideMockActions(() => actions$)]
+      providers: [
+        CoreEffects,
+        provideMockActions(() => actions$),
+        { provide: MatSnackBar, useValue: snackbarSpyDef },
+        { provide: MatDialog, useValue: dialogSpyDef }
+      ]
     });
 
     effects = TestBed.inject(CoreEffects);
+    snackBarSpy = TestBed.inject(MatSnackBar) as jasmine.SpyObj<MatSnackBar>;
+    dialogSpy = TestBed.inject(MatDialog) as jasmine.SpyObj<MatDialog>;
   });
 
   it('should be created', () => {
